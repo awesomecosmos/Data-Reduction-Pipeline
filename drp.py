06c1dacf9144d35978b5a6447e8f4c939d5694c5
Aayushi Verma <56360465+awesomecosmos@users.noreply.github.com>
2021-03-22 10:49:06 +1300

    update
    This version is producing an error 'TypeError: unsupported operand type(s) for *: 'numpy.ndarray' and 'IrreducibleUnit'' at line 445.

--- drp.py
+++ drp.py
@@ -26,7 +26,7 @@ warnings.filterwarnings('ignore')
 
 # Astropy packages
 from astropy.io import fits
-from astropy import units as u
+import astropy.units as u
 from astropy.stats import mad_std
 from astropy.nddata import CCDData
 from astropy.utils.data import get_pkg_data_filename
@@ -73,14 +73,14 @@ def find_nearest_dark_exposure(image, dark_exposure_times, tolerance=0.5):
     """
 
     dark_exposures = np.array(list(dark_exposure_times))
-    idx = np.argmin(np.abs(dark_exposures - image.header['exptime']))
+    idx = np.argmin(np.abs(dark_exposures - image.header['EXPTIME']))
     closest_dark_exposure = dark_exposures[idx]
 
     if (tolerance is not None and 
-        np.abs(image.header['exptime'] - closest_dark_exposure) > tolerance):
+        np.abs(image.header['EXPTIME'] - closest_dark_exposure) > tolerance):
         
         raise RuntimeError('Closest dark exposure time is {} for flat of exposure '
-                           'time {}.'.format(closest_dark_exposure, a_flat.header['exptime']))
+                           'time {}.'.format(closest_dark_exposure, a_flat.header['EXPTIME']))
         
     
     return closest_dark_exposure
@@ -97,7 +97,10 @@ def chip_num_extractor(img):
     * number of chip
       dtype: int 
     """
-    return abs(int(img[-6:-4]))
+    hdu1 = fits.open(img)
+    chip_num = hdu1[0].header['CHIP']
+    return chip_num
+    #return abs(int(img[-6:-4]))
 
 
 def chip_separator(IMAGElist):
@@ -248,12 +251,12 @@ for index,BIAS_chips in enumerate(BIAS_chips_files):
     # writing combined bias as a fits file
     master_bias.write(MBIAS_path / 'mbias_chip{}.fit'.format(chip_num),overwrite=True)
 
-    # plotting single bias compared to combined bias
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-    show_image(BIAS_ccd[0], cmap='gray', ax=ax1, fig=fig, percl=90)
-    ax1.set_title('Single calibrated bias for Chip {}'.format(chip_num))
-    show_image(master_bias.data, cmap='gray', ax=ax2, fig=fig, percl=90)
-    ax2.set_title('{} bias images combined for Chip {}'.format(num_of_biases,chip_num))
+    # # plotting single bias compared to combined bias
+    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+    # show_image(BIAS_ccd[0], cmap='gray', ax=ax1, fig=fig, percl=90)
+    # ax1.set_title('Single calibrated bias for Chip {}'.format(chip_num))
+    # show_image(master_bias.data, cmap='gray', ax=ax2, fig=fig, percl=90)
+    # ax2.set_title('{} bias images combined for Chip {}'.format(num_of_biases,chip_num))
 
 #%%
 ##-----------------------------CALIBRATING DARKS-----------------------------##
@@ -345,77 +348,248 @@ for DARK_chips in DARK_cal_chips_files:
                                                                   chip_num),
                                                              overwrite=True)
 
-        # plotting single dark compared to combined dark
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-        #plotting single dark for current chip
-        show_image(dark_ccd, cmap='gray', ax=ax1, fig=fig, percl=90)
-        ax1.set_title('Single calibrated dark for Chip {}'.format(chip_num))
-        # plotting combined dark for current chip
-        show_image(master_dark.data, cmap='gray', ax=ax2, fig=fig, percl=90)
-        ax2.set_title('{}s Master Dark for Chip {}'.format(exptime,chip_num))
+        # # plotting single dark compared to combined dark
+        # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+        # #plotting single dark for current chip
+        # show_image(dark_ccd, cmap='gray', ax=ax1, fig=fig, percl=90)
+        # ax1.set_title('Single calibrated dark for Chip {}'.format(chip_num))
+        # # plotting combined dark for current chip
+        # show_image(master_dark.data, cmap='gray', ax=ax2, fig=fig, percl=90)
+        # ax2.set_title('{}s Master Dark for Chip {}'.format(exptime,chip_num))
 
 
 #%%
 # ##--------------------------------FLATS-----------------------------------##
-# # (8/3/21) currently this section combines all the chips
-# # will need to sort this out
-# ##------------------------------##
-# # FLATS_path = Path('Obs Data/FLATS')
-# # FLATS_imgs = ImageFileCollection(FLATS_path, keywords='*')
 
-# # # (8/3/21) will need to add an earlier section which stores the exptime of 
-# # # the science image to put in here, instead of 300
-# # FLATS_files = FLATS_imgs.files_filtered(FIELD ='              flat',include_path=True)
+FLAT_path = Path('Obs Data/FLAT')
+# FLAT_path = Path('Obs Data_old/FLATS')
+to_include = ['/*-1.fit','/*-2.fit','/*-3.fit','/*-4.fit','/*-5.fit',
+              '/*-6.fit','/*-7.fit','/*-8.fit','/*-9.fit','/*-10.fit']
+good_files = []
+for i in to_include:
+    good_file = glob.glob('Obs Data/FLAT' + i)
+    # good_file = glob.glob('Obs Data_old/FLATS' + i)
+    good_files += good_file
 
-# # # converting each DARKS file to a fits array
-# # FLATS_fits = [fits.getdata(FLATS_file) for FLATS_file in FLATS_files]
-# # # converting each BIAS array to a CCDData object
-# # FLATS_ccd = [CCDData(FLATS_fit,unit=u.adu) for FLATS_fit in FLATS_fits]
+FLAT_imgs = ImageFileCollection(filenames=good_files)
 
+# FLAT_files = FLAT_imgs.files_filtered(FIELD ='        flat_round',
+#                                  include_path=True)
+FLAT_files = FLAT_imgs.files_filtered(FIELD ='              flat',
+                                  include_path=True)
+# making/checking MFLAT path/folder
+FLAT_cal_path = path_checker(FLAT_path,'Calibrated Flats')
+MFLAT_path = path_checker(FLAT_path,'Master Flats')
+FLAT_exptimes = exptime_checker(FLAT_files)
+FLAT_chips_files = chip_separator(FLAT_files)
 
-# pseudocode for first section of FLATS:
-# flats_exp_times = []
-# for FLAT in FLATS:
-    # exptime = exptime(from header)
-    # flats_exp_times.append(exptime)
+# FLAT_ccds = FLAT_imgs.ccds(FIELD ='        flat_round',            
+#                            ccd_kwargs={'unit': 'adu'},                            
+#                            return_fname=True)
 
-# change dir to DARKS
-# use calibrated darks path
-# for DARK in DARKS:
-    # exptime_darks = exptime(from header)
-    # for flat_exp in flats_exp_times:
-        # if flat_exp == exptime_darks:
-            # save DARK to new folder in FLATS folder called 'Cal Darks for Flats'
-        # else:
-            # pass
-#https://mwcraig.github.io/ccd-as-book/05-03-Calibrating-the-flats.html        
+FLAT_ccds = FLAT_imgs.ccds(FIELD ='              flat',            
+                            ccd_kwargs={'unit': 'adu'},                            
+                            return_fname=True)
+
+#%%
+# reading in master dark files from Master Darks folder
+# excluding non-science quicklook images and biases
+MDARK_imgs = ImageFileCollection(MDARK_path, keywords='*')
+MDARK_files = MDARK_imgs.files_filtered(SUBBIAS = 'ccd=<CCDData>, master=<CCDData>',
+                                        include_path=True)
+MDARK_ccds = MDARK_imgs.ccds(SUBBIAS = 'ccd=<CCDData>, master=<CCDData>', 
+                             combined=True)
+MDARK_chips_files = chip_separator(MDARK_files)
+
+n_combined_dark = len(MDARK_files)
+expected_exposure_times = set(FLAT_exptimes)
+actual_exposure_times = set(h['EXPTIME'] for h in MDARK_imgs.headers(SUBBIAS = 'ccd=<CCDData>, master=<CCDData>', combined=True))
+
+if (expected_exposure_times - actual_exposure_times):
+    raise RuntimeError('Encountered unexpected exposure time in combined darks. '
+                       'The unexpected times are {}'.format(actual_exposure_times - expected_exposure_times))
+
+combined_darks = {ccd.header['EXPTIME']: ccd for ccd in MDARK_ccds}
+
+#%%
+for f_index,FLAT_chips in enumerate(FLAT_chips_files):
+    # getting master darks for current chip
+    MDARK_chips_file = MDARK_chips_files[f_index]
+    
+    for FLAT_file in FLAT_chips:
+        hdu1 = fits.open(FLAT_file)
+        f_file_name = hdu1[0].header['RUN'].strip(' ')
+        flat_exptime = hdu1[0].header['EXPTIME']
+        f_obs_set = hdu1[0].header['SET'].strip(' ')
+        f_chip_num = hdu1[0].header['CHIP']
         
+        FLAT_ccd = CCDData(FLAT_file,unit=u.adu)
+        
+        for mdark in MDARK_chips_file:
+            mdark_hdu1 = fits.open(mdark)
+            mdark_exptime = mdark_hdu1[0].header['EXPTIME']
+            if mdark_exptime == flat_exptime:
+                MDARK_exptime = mdark_exptime
+                MDARK_to_subtract = CCDData(mdark,unit=u.adu)
+        
+        # closest_dark = find_nearest_dark_exposure(FLAT_ccd, actual_exposure_times)
 
+        # MDARK_exptime_u = u.Quantity(MDARK_exptime,u.second)
+        # flat_exptime_u = u.Quantity(flat_exptime,u.second)
+        MDARK_exptime_u = MDARK_exptime*u.second
+        flat_exptime_u = flat_exptime*u.second
+        # MDARK_to_subtract.header['EXPTIME'] = MDARK_exptime_u
+        
+        # header_exptime = ccdp.Keyword('EXPTIME', unit=u.second)
+        
+        calibrated_flat = ccdp.subtract_dark(ccd=FLAT_ccd, 
+                              master=MDARK_to_subtract,
+                              dark_exposure=MDARK_exptime_u,
+                              data_exposure=flat_exptime_u,
+                              # exposure_time='EXPTIME',
+                              exposure_unit=u.second,
+                              scale=False)
+        
+        # calibrated_flat = ccdp.flat_correct(MDARK_to_subtract,FLAT_ccd)
+                              
+
+#%%
+
+for flat_ccd, file_name in FLAT_ccds:   
+    
+    # extracting header data for display/saving purposes later
+    hdu1 = fits.open(flat_ccd)
+    file_name = hdu1[0].header['RUN'].strip(' ')
+    exptime = hdu1[0].header['EXPTIME']
+    obs_set = hdu1[0].header['SET'].strip(' ')
+    chip_num = hdu1[0].header['CHIP']
+    
+    # Find the correct dark exposure
+    closest_dark = find_nearest_dark_exposure(flat_ccd, actual_exposure_times)
+    
+    # Subtract the dark current 
+    calibrated_flat = ccdp.subtract_dark(flat_ccd, 
+                             combined_darks[closest_dark],
+                             exposure_time='exptime', 
+                             exposure_unit=u.second)
+
+    # Save the result; there are some duplicate file names so pre-pend "flat"
+    calibrated_flat.write(FLAT_cal_path / 
+              "calibrated_flat-{}-{}-{}-{}.fit".format(file_name,exptime,
+                                                       obs_set,chip_num),
+                                                       overwrite=True)
 
 
 #%%
 
-# ifc_reduced = ccdp.ImageFileCollection(DARKS_cal_path)
-# # combined_dark_files = ifc_reduced.files_filtered(EXPTIME=300,combined=True)
-# # combined_dark_files = ifc_reduced.files_filtered(SUBBIAS = 'ccd=<CCDData>, master=<CCDData>',combined=True)
-# combined_dark_files = ifc_reduced.files_filtered(SIMPLE=True,combined=True)
-# flat_image_type = '              flat'
-
-# #%%
-
-# n_combined_dark = len(combined_dark_files)
-# expected_exposure_times = set([5, 10, 20, 30])
-
-# if n_combined_dark < 10:
-#     raise RuntimeError('One or more combined dark is missing. Please re-run the dark notebook.')
-# elif n_combined_dark > 10:
-#     raise RuntimeError('There are more combined dark frames than expected.')
+for FLAT_chips in FLAT_chips_files:
+    # seperating list of files by their exposure lengths
+    exptime_seperated_files = exptime_separator(FLAT_chips)
     
-# actual_exposure_times = set(h['exptime'] for h in ifc_reduced.headers(imagetyp='dark', combined=True))
+    # for each array of files for each exposure length:
+    for exptime_seperated_exps in exptime_seperated_files:
+        # extracting header information for this set of files
+        hdu1 = fits.open(exptime_seperated_exps[0])
+        exptime = hdu1[0].header['EXPTIME']
+        chip_num = hdu1[0].header['CHIP']
+        
+        # getting CCD image for plotting purposes
+        flat_fits = fits.getdata(exptime_seperated_exps[0]) 
+        flat_ccd = CCDData(flat_fits,unit=u.adu) 
 
-# if (expected_exposure_times - actual_exposure_times):
-#     raise RuntimeError('Encountered unexpected exposure time in combined darks. '
-#                        'The unexpected times are {}'.format(actual_exposure_times - expected_exposure_times))
+        # combining all the darks of this set together
+        master_flat = ccdp.subtract_dark(a_flat_reduced, 
+                                         combined_darks[closest_dark], 
+                                         exposure_time='exptime', 
+                                         exposure_unit=u.second, 
+                                         scale=False)
+        
+        # writing keywords to header
+        master_flat.meta['combined'] = True
+        master_flat.meta['EXPTIME'] = exptime
+        master_flat.meta['CHIP'] = chip_num
+        
+        # writing combined dark as a fits file
+        master_flat.write(MFLAT_path / 'mflat-{}-chip{}.fit'.format(exptime,
+                                                                  chip_num),
+                                                              overwrite=True)
+
+
+
+
+# for FLAT_chips in FLAT_chips_files:
+#     # seperating list of files by their exposure lengths
+#     exptime_seperated_files = exptime_separator(FLAT_chips)
+    
+#     # for each array of files for each exposure length:
+#     for exptime_seperated_exps in exptime_seperated_files:
+#         # extracting header information for this set of files
+#         hdu1 = fits.open(exptime_seperated_exps[0])
+#         exptime = hdu1[0].header['EXPTIME']
+#         chip_num = hdu1[0].header['CHIP']
+        
+#         # getting CCD image for plotting purposes
+#         flat_fits = fits.getdata(exptime_seperated_exps[0]) 
+#         flat_ccd = CCDData(flat_fits,unit=u.adu) 
+
+#         # combining all the darks of this set together
+#         master_flat = ccdp.combine(exptime_seperated_exps,unit=u.adu,
+#                               method='average',
+#                               sigma_clip=True, 
+#                               sigma_clip_low_thresh=5, 
+#                               sigma_clip_high_thresh=5,
+#                               sigma_clip_func=np.ma.median, 
+#                               sigma_clip_dev_func=mad_std,
+#                               mem_limit=350e6)
+        
+#         # writing keywords to header
+#         master_flat.meta['combined'] = True
+#         master_flat.meta['EXPTIME'] = exptime
+#         master_flat.meta['CHIP'] = chip_num
+        
+#         # writing combined dark as a fits file
+#         master_flat.write(MFLAT_path / 'mflat-{}-chip{}.fit'.format(exptime,
+#                                                                   chip_num),
+#                                                              overwrite=True)
+
+#         # plotting single dark compared to combined dark
+#         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+#         #plotting single dark for current chip
+#         show_image(flat_ccd, cmap='gray', ax=ax1, fig=fig, percl=90)
+#         ax1.set_title('Single calibrated flat for Chip {}'.format(chip_num))
+#         # plotting combined dark for current chip
+#         show_image(master_flat.data, cmap='gray', ax=ax2, fig=fig, percl=90)
+#         ax2.set_title('{}s Master Flat for Chip {}'.format(exptime,chip_num))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
